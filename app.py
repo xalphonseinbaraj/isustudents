a6cc9543f20154bd2e57f10567b3f704c3bda90a
xalphonseinbaraj <47241538+xalphonseinbaraj@users.noreply.github.com>
2021-06-17 11:35:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,6 @@ def who():
     return "Congratulations, this is another way to show HIII!!!!!"
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run()
 
 
